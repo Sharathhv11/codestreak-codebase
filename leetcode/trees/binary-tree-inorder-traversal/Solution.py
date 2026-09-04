a301b141e65a1599d735c18f3bb5823ed706fc2b
Sharath <132804379+Sharathhv11@users.noreply.github.com>
2026-09-04 22:22:22 +0530

    Sync Solution for Binary Tree Inorder Traversal [trees] - CodeStreak

---
+++ leetcode/trees/binary-tree-inorder-traversal/Solution.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        arr = []
+        def helper(root):
+            
+            if( root is None ):
+                return
+
+            helper(root.left)
+            arr.append(root.val)
+            helper(root.right)
+        helper(root)
+        return arr
+        
