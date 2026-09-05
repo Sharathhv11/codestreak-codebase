85f5ebb8b15ff465fded2a2cf0d649bef1fbba84
Sharath <132804379+Sharathhv11@users.noreply.github.com>
2026-09-05 22:51:13 +0530

    Sync Solution for Binary Tree Level Order Traversal [bfs] - CodeStreak

---
+++ leetcode/bfs/binary-tree-level-order-traversal/Solution.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if( not root ):
+            return []
+        queue = [root]
+        result = []
+
+        while( queue ):
+            n = len(queue)
+
+            res = []
+            for i in range(n):
+                node = queue[i]
+                res.append(node.val)
+                if( node.left ):
+                    queue.append(node.left)
+                if( node.right ):
+                    queue.append(node.right)
+            queue = queue[n:]
+            result.append(res)
+
+        return result
+
